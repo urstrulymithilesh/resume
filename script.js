3f3693aeaee3c9908ecd73c48bad24e84f54d388
urstrulymithilesh <91820167+urstrulymithilesh@users.noreply.github.com>
2025-01-08 16:42:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
+document.querySelectorAll('.nav-bar a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
         const targetId = link.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
 
